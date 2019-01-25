1efaa9c5a2e8cdfce063417a8f3d63eaaeb2cbc7
Nicholas Lanotte <Njlanotte@wpi.edu>
2019-01-25 02:53:11 -0500

    added launch files for testing base station and raspberry pi computer vision

--- demining_mqp/ROSNodes/IRCamera.py
+++ demining_mqp/ROSNodes/IRCamera.py
@@ -25,10 +25,12 @@ class IRCam:
                 np.right_shift(lepton_buf, 8, lepton_buf)
                 lepton_buf = np.uint8(lepton_buf)
                 finalImage = image
+                index = 0
                 for x in lepton_buf:
                     for y in x:
                         for z in y:
-                            finalImage.data.append(z)
+                            finalImage.data[index] = z
+                            index = index +1
                 print(finalImage)
                 self._receivedImage.publish(finalImage)
 
