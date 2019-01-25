02d3d23e78d95c0708f2aa33fb8734c9d7969f8f
Nicholas Lanotte <Njlanotte@wpi.edu>
2019-01-25 02:07:27 -0500

    added launch files for testing base station and raspberry pi computer vision

--- demining_mqp/ROSNodes/CompVision.py
+++ demining_mqp/ROSNodes/CompVision.py
@@ -18,7 +18,7 @@ class CompVisionNode:
 
         # must be gray-scale image which it should be from the camera
         gray = data.data  # make this equal to the image from the camera
-        print gray
+        print len(gray)
         reconstruct = np.zeros((60, 80, 1), dtype=np.uint8)
         index = 0
         for x in range (0,80):
