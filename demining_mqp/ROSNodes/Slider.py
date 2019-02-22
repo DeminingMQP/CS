eb87a61ce63bae5c3e6716c8a69d6420fbc5c352
Dillon Arnold <djarnold@wpi.edu>
2019-02-22 02:01:07 -0500

    Slider code board switched to bcm

--- demining_mqp/ROSNodes/Slider.py
+++ demining_mqp/ROSNodes/Slider.py
@@ -14,7 +14,7 @@ class slider:
         self.MotorSpeedPin = 37
         #self.MotorStallPin = 20
         self.CurrentMotorDirection = 1 # 0 for left, 1 for right? whatever you want
-        GPIO.setmode(GPIO.Board)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.RightLimitSwitch, GPIO.IN)
         GPIO.setup(self.RightLimitSwitch, GPIO.IN)
         GPIO.setup(self.MotorStallPin, GPIO.IN)
