9adb910e7e2778b6a4c93f4a9a3862c64ab2bae8
Nicholas Lanotte <Njlanotte@wpi.edu>
2019-04-03 17:26:08 -0400

    Got four bar code working. Need to add limit switch

--- demining_mqp/ROSNodes/FourBarControl.py
+++ demining_mqp/ROSNodes/FourBarControl.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 import rospy, math
-from std_msgs.msg import Bool
+from std_msgs.msg import *
 import numpy as np
 import RPi.GPIO as GPIO
 import fourbarposition
 from demining_mqp.msg import*
 
 
+
 class fourbar:
-    UP = True
-    DOWN = False
 
     def __init__(self):
         self._sendFourBarData = rospy.Publisher('/FourBarPos', fourbarposition,
                                                 queue_size=5)
-        self.metalDetectorListener = rospy.Subscriber('/calibrate', bool, self.calibrateCallback)
+        self.metalDetectorListener = rospy.Subscriber('/calibrate', std_msgs.msg.Bool, self.calibrateCallback)
         # send to nav system with a new message type
         self._sendObstacle = rospy.Publisher('/SteepSlope', Bool, queue_size=1)
         self.upPin = 12
@@ -27,28 +26,32 @@ class fourbar:
         self.Motor2EncPin2 = 29
         self.Motor2SpeedPin = 32
         self.Motor2DirPin = 33
-        GPIO.setmode(GPIO.Board)
+        self.UP = True
+        self.DOWN = False
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.upPin, GPIO.IN)  # need to pick a pinbases
         GPIO.setup(self.downPin, GPIO.IN)  # need to pick a pin
         GPIO.setup(self.Motor1DirPin, GPIO.OUT)
         GPIO.setup(self.Motor2DirPin, GPIO.OUT)
+        GPIO.setup(self.Motor1SpeedPin, GPIO.OUT)
+        GPIO.setup(self.Motor2SpeedPin, GPIO.OUT)
 
-        GPIO.add_event_detect(15, GPIO.RISING, callback=self.motor1Enc1Callback(), bouncetime=5)
-        GPIO.add_event_detect(18, GPIO.RISING, callback=self.motor1Enc2Callback(), bouncetime=5)
-        GPIO.add_event_detect(31, GPIO.RISING, callback=self.motor2Enc1Callback(), bouncetime=5)
-        GPIO.add_event_detect(33, GPIO.RISING, callback=self.motor2Enc2Callback(), bouncetime=5)
+        #GPIO.add_event_detect(15, GPIO.RISING, callback=self.motor1Enc1Callback(), bouncetime=5)
+        #GPIO.add_event_detect(18, GPIO.RISING, callback=self.motor1Enc2Callback(), bouncetime=5)
+        #GPIO.add_event_detect(31, GPIO.RISING, callback=self.motor2Enc1Callback(), bouncetime=5)
+        #GPIO.add_event_detect(33, GPIO.RISING, callback=self.motor2Enc2Callback(), bouncetime=5)
         
-        self.pwm1 = GPIO.PWM(16, 50)
-        self.pwm2 = GPIO.PWM(32, 50)
-        self.pwm1.start(0)
-        self.pwm2.start(0)
+        #self.pwm1 = GPIO.PWM(self.Motor1SpeedPin, 50)
+        #self.pwm2 = GPIO.PWM(self.Motor2SpeedPin, 50)
+        #self.pwm1.start(0)
+        #self.pwm2.start(0)
 
         self.m1e1Value = 0
         self.m1e2Value = 0
         self.m2e1Value = 0
         self.m2e2Value = 0
         self.m1dir = 1
-        self.fbp = fourbarposition(8, 40.76, 14.83, 24, 5, 3)
+        #self.fbp = fourbarposition(8, 40.76, 14.83, 24, 5, 3)
 
     def checkAlign(self):
         # Copy encoder values
@@ -59,12 +62,12 @@ class fourbar:
         if abs(m1Enc - m2Enc) > tolerance:
             if m2Enc > m1Enc:
                 self.pwm2.stop()
-                GPIO.output(self.Motor1DirPin, UP)
+                GPIO.output(self.Motor1DirPin, self.UP)
                 self.pwm1.ChangeDutyCycle(50)
                 self.checkAlign()
             elif m1Enc > m2Enc:
                 self.pwm1.stop()
-                GPIO.output(self.Motor2DirPin, UP)
+                GPIO.output(self.Motor2DirPin, self.UP)
                 self.pwm2.ChangeDutyCycle(50)
                 self.checkAlign()
 
@@ -76,22 +79,25 @@ class fourbar:
             self.pwm1.stop()
             self.pwm2.stop()
         elif upPin:
-            self.m1dir = UP
+            self.m1dir = self.UP
             GPIO.output(self.Motor1DirPin, self.m1dir)
-            GPIO.output(self.Motor2DirPin, not self.m1dir)
-            self.pwm1.ChangeDutyCycle(50)
-            self.pwm2.ChangeDutyCycle(50)
+            GPIO.output(self.Motor2DirPin, self.m1dir)
+            #GPIO.output(self.Motor1SpeedPin, GPIO.HIGH)
+            #GPIO.output(self.Motor2SpeedPin, GPIO.HIGH)
+            print("Raising")
         elif downPin:
-            self.m1dir = DOWN
+            self.m1dir = self.DOWN
             GPIO.output(self.Motor1DirPin, self.m1dir)
-            GPIO.output(self.Motor2DirPin, not self.m1dir)
-            self.pwm1.ChangeDutyCycle(50)
-            self.pwm2.ChangeDutyCycle(50)
+            GPIO.output(self.Motor2DirPin, self.m1dir)
+            #GPIO.output(self.Motor1SpeedPin, GPIO.HIGH)
+            #GPIO.output(self.Motor2SpeedPin, GPIO.HIGH)
+            print("Lowering")
         else:
-            self.pwm1.stop()
-            self.pqm2.stop()
-        self.fbp.calcPos(self.m1e1Value)
-        self._sendFourBarData(self.fbp)
+            GPIO.output(self.Motor1SpeedPin, GPIO.LOW)
+            GPIO.output(self.Motor2SpeedPin, GPIO.LOW)
+            print("Stopped")
+        #self.fbp.calcPos(self.m1e1Value)
+        #self._sendFourBarData(self.fbp)
 
 
     def motor1Enc1Callback(self):
@@ -118,12 +124,15 @@ if __name__ == '__main__':
     rospy.init_node('FourBarControl')
     FourBarControl = fourbar()
     rospy.sleep(1)
-    then = rospy.Time.now()
+    #then = rospy.Time.now()
 
     while not rospy.is_shutdown():
         rospy.sleep(0.02)
-        now = rospy.Time.now()
-        if (now.secs - then.secs) > 10:
-            FourBarControl.checkAlign()
-            then = now
+        #now = rospy.Time.now()
+        #if (now.secs - then.secs) > 10:
+         #   FourBarControl.checkAlign()
+          #  then = now
         FourBarControl.checkPins()
+    GPIO.output(FourBarControl.Motor1SpeedPin, GPIO.LOW)
+    GPIO.output(FourBarControl.Motor2SpeedPin, GPIO.LOW)
+    print("Ended")
