4716ed704d60e0ae2026e22d3a2cf16dc1a4118d
Ben Wagner <bpwagner@wpi.edu>
2019-04-04 20:19:57 -0400

    added limit switch functionality to stop motors at max rise angle.  Navigation needs to subscribe to topic /SteepSlope

--- demining_mqp/ROSNodes/FourBarControl.py
+++ demining_mqp/ROSNodes/FourBarControl.py
@@ -11,11 +11,10 @@ from demining_mqp.msg import*
 class fourbar:
 
     def __init__(self):
-        self._sendFourBarData = rospy.Publisher('/FourBarPos', fourbarposition,
-                                                queue_size=5)
+        self._sendFourBarData = rospy.Publisher('/FourBarPos', fourbarposition, queue_size=5)
         self.metalDetectorListener = rospy.Subscriber('/calibrate', std_msgs.msg.Bool, self.calibrateCallback)
         # send to nav system with a new message type
-        self._sendObstacle = rospy.Publisher('/SteepSlope', Bool, queue_size=1)
+        self._sendLimit = rospy.Publisher('/SteepSlope', std_msgs.msg.Bool, queue_size=1)
         self.upPin = 12
         self.downPin = 11
         self.Motor1EncPin1 = 15
@@ -28,6 +27,7 @@ class fourbar:
         self.Motor2DirPin = 33
         self.UP = True
         self.DOWN = False
+        self.MaxLimitSwitchPin = 22
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.upPin, GPIO.IN)  # need to pick a pinbases
         GPIO.setup(self.downPin, GPIO.IN)  # need to pick a pin
@@ -35,12 +35,14 @@ class fourbar:
         GPIO.setup(self.Motor2DirPin, GPIO.OUT)
         GPIO.setup(self.Motor1SpeedPin, GPIO.OUT)
         GPIO.setup(self.Motor2SpeedPin, GPIO.OUT)
+        GPIO.setup(self.MaxLimitSwitchPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        #GPIO.add_event_detect(self.MaxLimitSwitchPin, GPIO.RISING, callback=self.limitCallback())
 
         #GPIO.add_event_detect(15, GPIO.RISING, callback=self.motor1Enc1Callback(), bouncetime=5)
         #GPIO.add_event_detect(18, GPIO.RISING, callback=self.motor1Enc2Callback(), bouncetime=5)
         #GPIO.add_event_detect(31, GPIO.RISING, callback=self.motor2Enc1Callback(), bouncetime=5)
         #GPIO.add_event_detect(33, GPIO.RISING, callback=self.motor2Enc2Callback(), bouncetime=5)
-        
+
         #self.pwm1 = GPIO.PWM(self.Motor1SpeedPin, 50)
         #self.pwm2 = GPIO.PWM(self.Motor2SpeedPin, 50)
         #self.pwm1.start(0)
@@ -74,28 +76,33 @@ class fourbar:
     def checkPins(self):
         upPin = GPIO.input(self.upPin)
         downPin = GPIO.input(self.downPin)
-        if upPin and downPin:
-            self._sendObstacle.publish(True)
-            self.pwm1.stop()
-            self.pwm2.stop()
-        elif upPin:
-            self.m1dir = self.UP
-            GPIO.output(self.Motor1DirPin, self.m1dir)
-            GPIO.output(self.Motor2DirPin, self.m1dir)
-            #GPIO.output(self.Motor1SpeedPin, GPIO.HIGH)
-            #GPIO.output(self.Motor2SpeedPin, GPIO.HIGH)
-            print("Raising")
-        elif downPin:
-            self.m1dir = self.DOWN
-            GPIO.output(self.Motor1DirPin, self.m1dir)
-            GPIO.output(self.Motor2DirPin, self.m1dir)
-            #GPIO.output(self.Motor1SpeedPin, GPIO.HIGH)
-            #GPIO.output(self.Motor2SpeedPin, GPIO.HIGH)
-            print("Lowering")
-        else:
+
+        if self.MaxLimitSwitchPin == HIGH:
             GPIO.output(self.Motor1SpeedPin, GPIO.LOW)
             GPIO.output(self.Motor2SpeedPin, GPIO.LOW)
-            print("Stopped")
+        else:
+            if upPin and downPin:
+                self._sendObstacle.publish(True)
+                self.pwm1.stop()
+                self.pwm2.stop()
+            elif upPin:
+                self.m1dir = self.UP
+                GPIO.output(self.Motor1DirPin, self.m1dir)
+                GPIO.output(self.Motor2DirPin, self.m1dir)
+                #GPIO.output(self.Motor1SpeedPin, GPIO.HIGH)
+                #GPIO.output(self.Motor2SpeedPin, GPIO.HIGH)
+                print("Raising")
+            elif downPin:
+                self.m1dir = self.DOWN
+                GPIO.output(self.Motor1DirPin, self.m1dir)
+                GPIO.output(self.Motor2DirPin, self.m1dir)
+                #GPIO.output(self.Motor1SpeedPin, GPIO.HIGH)
+                #GPIO.output(self.Motor2SpeedPin, GPIO.HIGH)
+                print("Lowering")
+            else:
+                GPIO.output(self.Motor1SpeedPin, GPIO.LOW)
+                GPIO.output(self.Motor2SpeedPin, GPIO.LOW)
+                print("Stopped")
         #self.fbp.calcPos(self.m1e1Value)
         #self._sendFourBarData(self.fbp)
 
@@ -118,6 +125,11 @@ class fourbar:
         self.m1e2Value = 0
         self.m2e1Value = 0
         self.m2e2Value = 0
+
+    def limitCallback(self):
+        GPIO.output(self.Motor1SpeedPin, GPIO.LOW)
+        GPIO.output(self.Motor2SpeedPin, GPIO.LOW)
+        self._sendLimit.publish(True)
 
 
 if __name__ == '__main__':
